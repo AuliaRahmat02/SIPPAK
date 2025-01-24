7daa724899082d23f2a8a9c00d1932e2b2e53059
AuliaRahmat02 <91918758+AuliaRahmat02@users.noreply.github.com>
2025-01-24 14:41:03 +0700

    perbaikan model user dan ctt

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Support\Str;
-// use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model dsds;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
